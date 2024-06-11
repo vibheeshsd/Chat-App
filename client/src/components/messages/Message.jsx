72de234de8dc463920799fc1763cbce4f718f8dd
vibheeshsd <vibheesh281.pec@gmail.com>
2024-06-11 23:13:07 +0530

    deployment

---
+++ client/src/components/messages/Message.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
+import { extractTime } from "../../utils/extractTime";
+
+const Message = ({message}) => {
+
+  const {authUser} = useAuthContext();
+  const {selectedConversation} = useConversation();
+
+  const fromME = message.senderId === authUser._id;
+  const chatClassName = fromME ? 'chat-end' : 'chat-start' ;
+  const profilePic = fromME ? authUser.profilePic : selectedConversation.profilePic;
+  const bubbleBgcolor = fromME ? ' bg-blue-500': "";
+  const formattedTime = extractTime(message.createdAt);
+
+  const shakeClass = message.shouldShake ? "shake" : ""
+
+  return (
+    <div className={`chat ${chatClassName}`}>
+      <div className=" chat-image avatar">
+        <div className=" w-10 rounded-full">
+          <img
+            src={profilePic}
+            alt="Tailwind CSS chat bubble component"
+          />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white ${bubbleBgcolor} ${shakeClass} pb-2`}>{message.message}</div>
+      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
+    </div>
+  );
+};
+
+export default Message;
