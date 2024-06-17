dbcc985148aaad5f0a31b791c1454fa4a50ea3f9
vibheeshsd <104817943+vibheeshsd@users.noreply.github.com>
2024-06-17 18:50:02 +0530

    Deployment Changes

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://localhost:5000", {
+			const socket = io("https://chat-app-7kyo.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
